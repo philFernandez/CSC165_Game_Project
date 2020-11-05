5e734e09ba774be616c16e6bea51cef0b1124d8b
philFernandez <norcal618@gmail.com>
2020-11-04 18:20:27 -0800

    extra move back

--- Client/jsScripts/InitParams.js
+++ Client/jsScripts/InitParams.js
@@ -1,6 +1,5 @@
 // A place for avatar and game parameters (can change on the fly)
-var spinSpeed = 0.05;
-var avatarMoveBack = 6.0;
+var avatarMoveBack = 20.0;
 var playerAvatarPOS = new Object();
 playerAvatarPOSx = 0.0;
 playerAvatarPOSy = 0.0;
